400c02283c5f60cd991979bdf583d791e15af460
d1masssss <dmidyan@gmail.com>
2018-10-06 14:19:08 +0700

    Terbilang

---
+++ TerbilangApp/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace TerbilangApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Konversi = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputNominal = new System.Windows.Forms.TextBox();
+            this.hasilKonversi = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Konversi
+            // 
+            this.btn_Konversi.Location = new System.Drawing.Point(279, 97);
+            this.btn_Konversi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Konversi.Name = "btn_Konversi";
+            this.btn_Konversi.Size = new System.Drawing.Size(151, 28);
+            this.btn_Konversi.TabIndex = 0;
+            this.btn_Konversi.Text = "Cek Terbilang";
+            this.btn_Konversi.UseVisualStyleBackColor = true;
+            this.btn_Konversi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 103);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nominal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 133);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Terbilang";
+            // 
+            // inputNominal
+            // 
+            this.inputNominal.Location = new System.Drawing.Point(105, 101);
+            this.inputNominal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.inputNominal.Name = "inputNominal";
+            this.inputNominal.Size = new System.Drawing.Size(132, 22);
+            this.inputNominal.TabIndex = 3;
+            // 
+            // hasilKonversi
+            // 
+            this.hasilKonversi.FormattingEnabled = true;
+            this.hasilKonversi.ItemHeight = 16;
+            this.hasilKonversi.Location = new System.Drawing.Point(105, 133);
+            this.hasilKonversi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.hasilKonversi.Name = "hasilKonversi";
+            this.hasilKonversi.Size = new System.Drawing.Size(475, 116);
+            this.hasilKonversi.TabIndex = 4;
+            this.hasilKonversi.SelectedIndexChanged += new System.EventHandler(this.hasilKonversi_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 11);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(192, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nama    : Dimas Midyan Alam";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 39);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "NIM       : 17.11.0982";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 68);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Kelas     : S1-IF-01";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 321);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.hasilKonversi);
+            this.Controls.Add(this.inputNominal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Konversi);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Konversi Nilai";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Konversi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputNominal;
+        private System.Windows.Forms.ListBox hasilKonversi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
